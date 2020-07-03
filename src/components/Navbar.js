bc6743b8dba8b499cc261018d506156d402f673f
vickean <vickean@gmail.com>
2020-07-03 15:09:34 +0800

    implemented consumption of multiple contexts in a single component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Navbar = (props) => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
 
   const theme = isLightTheme ? light : dark;
 
   return (
     <nav style={{ background: theme.ui, color: theme.syntax }}>
       <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? "Logged in" : "Logged out"}
+      </div>
       <ul>
         <li>Home</li>
         <li>About</li>
