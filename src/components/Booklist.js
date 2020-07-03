997e95e532084cff435253627c3f9345297d88e9
vickean <vickean@gmail.com>
2020-07-03 12:16:39 +0800

    done with introduction

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookList = (props) => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the way of kings</li>
+        <li>the name of the wind</li>
+        <li>the final empire</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
